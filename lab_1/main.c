6fd22f59f5fbc4291766e63bfd7758f130224a45
minicaramelka <3001Alex01@gmail.com>
2021-10-06 22:08:47 +0300

    Загружена лаба номер 1

---
+++ lab_1/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void handler(){
+	printf("Потомок %d и родитель %d завершины.\n", getpid(), getppid());
+}
+
+int main(){
+	pid_t pid;
+	int exit_stat = 0;
+	atexit(handler);
+	switch(pid=fork()) {
+	case -1:
+		exit(1);
+	case 0:
+		printf(" CHILD: Это процесс-потомк!\n");
+		printf(" CHILD: Мой  PID -- %d\n", getpid());
+		printf(" CHILD: PID моего родителя -- %d\n", getppid());
+		printf(" CHILD: Выход!\n");
+		exit(exit_stat);
+	default:
+		printf("PARENT: Это процесс-родитель!\n");
+		printf("PARENT: Мой PID -- %d\n", getpid());
+		printf("PARENT: PID моего родителя %d\n",pid);
+		printf("PARENT: Я жду, пока потомк не вызовет exit()...\n"); wait(&exit_stat);
+		printf("PARENT: Код возврата потомка:%d\n", WEXITSTATUS(exit_stat));
+		printf("PARENT: Выход!\n");
+	}
+	return 0;
+}
+
