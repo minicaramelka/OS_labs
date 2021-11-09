9ed2cb9e7029ea8c07292ab5f67fa77218b442eb
minicaramelka <59147993+minicaramelka@users.noreply.github.com>
2021-11-10 01:05:33 +0300

    Исправления по последним правкам
    Больше не используется дополнительный файл Stat.txt
    Добавлена обработка некоторых ошибок

--- lab_0.7/main.c
+++ lab_0.7/main.c
@@ -9,56 +9,52 @@
 #include <stddef.h>
 
 int input(char* archive, char* inputFile){
-	FILE *arch, *file, *statInfo;
+	FILE *arch, *file;
 	char currentSymbol;
 	int fileSize = 0;
-	if((arch = fopen(archive, "a+")) !=NULL){
+	int archSize = 0;
+	int bufFileSize = 0;
+	char* bufFileName;
+	bufFileName = malloc(strlen(inputFile));
+	if((arch = fopen(archive, "a+")) != NULL){
 		if((file = fopen(inputFile, "r")) != NULL){
-			statInfo = fopen("Stat.txt", "a+");
-		        size_t counter = 0;
-        		int ch;
-        		for (ch = getc(statInfo); ch != EOF; ch = getc(statInfo)){
-                		if(ch == '\n' )
-                        	counter = counter + 1;
-        		}
-        		fseek(statInfo, 0, SEEK_SET);
-			if(counter > 0){
-                        	struct info{
-                                	char fileName[1024];
-                                	int fileSize;
-                        	};
-                        	struct info ar[counter];
-                        	char i = 0;
-                        	int find = 0;
-                        	while (!feof(statInfo) && i != counter){
-                                	fscanf(statInfo, "%d %s", &ar[i].fileSize, ar[i].fileName);
-                                	if(strcmp(inputFile, ar[i].fileName) == 0){
+			fseek(arch, 0, SEEK_END);
+                        archSize = ftell(arch);
+			fseek(arch, 0, SEEK_SET);
+			if(archSize != 0){
+				int find = 0;
+				while(!feof(arch)){
+					fscanf(arch, "%d %s", &bufFileSize, bufFileName);
+					if(strcmp(inputFile, bufFileName) == 0){
                                         	++find;
-                                        	break;
+                                		break;
                                 	}
-                                	else
-                                        	i++;
-                        	}
-                        	if(find > 0){
-                                	printf("Файл с таким имененм уже есть\n");
-					return 0;
+					else{
+						for(int i = 0; i < bufFileSize; i++){
+							fgetc(arch);
+						}
+					}
 				}
+				if(find > 0){
+                                        printf("Файл с таким имененм уже есть\n");
+                                        return 0;
+                                }
 				else{
-					if(file == NULL)
+                                	if(file == NULL)
                                         	fileSize = 0;
                                 	else{
                                         	fseek(file, 0, SEEK_END);
                                         	fileSize = ftell(file);
                                 	}
                                 	fseek(file, 0, SEEK_SET);
+                                	fprintf(arch, "%d %s\n", fileSize, inputFile);
                                 	while (!feof(file)){
                                         	currentSymbol = fgetc(file);
                                         	if (currentSymbol == EOF)
                                                 	break;
                                         	fprintf(arch, "%c", currentSymbol);
                                 	}
-                                	statInfo = fopen("Stat.txt", "a");
-                                	fprintf(statInfo, "%d %s\n", fileSize, inputFile);
+					fclose(file);
 				}
 			}
 			else{
@@ -69,156 +65,151 @@ int input(char* archive, char* inputFile){
 					fileSize = ftell(file);
 				}
 				fseek(file, 0, SEEK_SET);
+				fprintf(arch, "%d %s\n", fileSize, inputFile);
 				while (!feof(file)){
         				currentSymbol = fgetc(file);
         				if (currentSymbol == EOF)
         					break;
         				fprintf(arch, "%c", currentSymbol);
 				}
-				statInfo = fopen("Stat.txt", "a");
-				fprintf(statInfo, "%d %s\n", fileSize, inputFile);
+				fclose(file);
 			}
 		}
-		fclose(file);
+		else
+			printf("Нет такого файла\n");
         	fclose(arch);
-		fclose(statInfo);
 	}
 	else{
+		free(bufFileName);
 		printf("Ошибка!\n");
 		return -1;
 	}
+	free(bufFileName);
 }
 
 void extract(char* archive, char* outputFile){
-	FILE *arch, *file, *statInfo;
-	statInfo = fopen("Stat.txt", "a+");
-	size_t counter = 0;
-        int ch;
-	fseek(statInfo, 0, SEEK_SET);
-        for (ch = getc(statInfo); ch != EOF; ch = getc(statInfo)){
-                if(ch == '\n' )
-                        counter = counter + 1;
-        }
-        if(counter > 0){
-		if((file = fopen(outputFile, "w")) != NULL){
-			struct info{
-        			char fileName[1024];
-                		int fileSize;
-        		};
-        		struct info helpArch[counter];
-        		char findPos = 0;
+	FILE *arch, *file;
+	int archSize = 0;
+	int bufFileSize = 0;
+        char* bufFileName;
+	if((arch = fopen(archive, "r")) != NULL){
+        	bufFileName = malloc(strlen(outputFile));
+		fseek(arch, 0, SEEK_END);
+        	archSize = ftell(arch);
+        	fseek(arch, 0, SEEK_SET);
+		char* beforeFind = 0;
+                int beforeSize = 0;
+		if(archSize > 0){
 			int find = 0;
-			char* beforeFind = 0;
-			int beforeSize = 0;
-			fseek(statInfo, 0, SEEK_SET);
-        		while (!feof(statInfo) && findPos != counter){
-        			fscanf(statInfo, "%d %s", &helpArch[findPos].fileSize, helpArch[findPos].fileName);
-                		if(strcmp(outputFile, helpArch[findPos].fileName) == 0){
-					++find;
-					break;
+			int counter = 1;
+                	while(!feof(arch)){
+                		fscanf(arch, "%d %s", &bufFileSize, bufFileName);
+				int bufSize = bufFileSize;
+                        	if(strcmp(outputFile, bufFileName) == 0){
+                                        while(bufSize / 10 > 0){
+                                                bufSize = bufSize /10;
+                                                counter++;
+                                        }
+                        		++find;
+                                	break;
+                        	}
+                        	else{
+        	                        while(bufSize / 10 > 0){
+						bufSize = bufSize /10;
+	                                        counter++;
+					}
+                	                beforeSize = beforeSize + bufFileSize + strlen(bufFileName) + counter + 2;
+                        		for(int i = 0; i < bufFileSize; i++){
+                                		fgetc(arch);
+                                	}
+					counter = 1;
 				}
-				else{
-					beforeSize = beforeSize + helpArch[findPos].fileSize;
-                			findPos++;
-				}
-        		}
-			if(find > 0){
-				int archSize = 0;
-				char i;
-				struct info ar[counter];
-				int outputSize = helpArch[findPos].fileSize;
-				char* delAr[outputSize];
-				fseek(statInfo, 0, SEEK_SET);
-				while (!feof(statInfo) && i != counter){
-					fscanf(statInfo, "%d %s", &ar[i].fileSize, ar[i].fileName);
-					archSize = archSize + ar[i].fileSize;
-					i++; 
-				}
+                	}
+                	if(find > 0){
+				file = fopen(outputFile, "w");
+				char* delAr[bufFileSize];
 				char* afterFind = 0;
-                                int afterSize = archSize - beforeSize - outputSize;
-				if((arch = fopen(archive, "r")) != NULL){
-					if(beforeSize != 0){
-						beforeFind = malloc(beforeSize);
-						fread(beforeFind, 1, beforeSize, arch);
-					}
-					fread(delAr, outputSize, 1, arch);
-					fwrite(delAr, outputSize, 1, file);
-					if(afterSize != 0){
-						afterFind = malloc(afterSize);
-						fread(afterFind, 1, afterSize, arch);
-					}
-					fclose(arch);
-				}
+				int afterSize = archSize - beforeSize - bufFileSize - strlen(bufFileName) - counter - 2;
+				fseek(arch, 0, SEEK_SET);
+				if(beforeSize != 0){
+                                        beforeFind = malloc(beforeSize);
+                                        fread(beforeFind, 1, beforeSize, arch);
+                                }
+				int bufStrSize = strlen(bufFileName) + counter + 2;
+				for(int i = 0; i < bufStrSize; i++){
+                                                fgetc(arch);
+                                }
+				fread(delAr, bufFileSize, 1, arch);
+                                fwrite(delAr, bufFileSize, 1, file);
+                                if(afterSize != 0){
+                                        afterFind = malloc(afterSize);
+                                        fread(afterFind, 1, afterSize, arch);
+                                }
+				fclose(arch);
 				if((arch = fopen(archive, "w")) != NULL){
-					if(beforeFind != 0)
-						fwrite(beforeFind, beforeSize, 1, arch);
-					if(afterFind !=0)
-						fwrite(afterFind, afterSize, 1, arch);
-					fclose(arch);
-				}
-				fclose(statInfo);
-				if((statInfo = fopen("Stat.txt", "w")) != NULL){
-					fseek(statInfo, 0, SEEK_SET);
-					for(int i = 0; i < counter; i++){
-						if(strcmp(ar[i].fileName, helpArch[findPos].fileName) != 0){
-							fprintf(statInfo, "%d %s\n", ar[i].fileSize, ar[i].fileName);
-						}
-					}
-					fclose(statInfo);	
-				}
-			}		
+                                	if(beforeFind != 0)
+                                        	fwrite(beforeFind, beforeSize, 1, arch);
+                                	if(afterFind !=0)
+                                        	fwrite(afterFind, afterSize, 1, arch);
+                                	fclose(arch);
+                        	}
+				fclose(file);
+			}
 			else
-				printf("Нет такого файла\n");
-			fclose(file);
+                        	printf("Нет такого файла\n");
 		}
 		else
-                	printf("Нет такого файла\n");
+			printf("Архив пустой\n");
 	}
 	else
-		printf("Архив пустой\n");
+		printf("Нет такого архива\n");
+	
 }
 
-void archStat(){
-	FILE *statInfo;
-	if(statInfo = fopen("Stat.txt", "r")){
-		size_t counter = 0;
-		int ch;
-		for (ch = getc(statInfo); ch != EOF; ch = getc(statInfo)){
-        		if(ch == '\n' ) 
-        			counter = counter + 1; 
-    		}       
-		printf("Количество файлов в архиваторе: %d\n", counter);
-		fseek(statInfo, 0, SEEK_SET);
-		if(counter > 0){
-			size_t archSize = 0;
-			struct info{
-				char fileName[1024];
-				int fileSize;
-			};
-			struct info arch[counter];
-			char i = 0;
+
+void archStat(char* archive){
+	FILE *arch;
+	int bufFileSize = 0;
+        char bufFileName[1024];
+	int archSize = 0;
+	if((arch = fopen(archive, "r")) != NULL){
+		fseek(arch, 0, SEEK_END);
+                archSize = ftell(arch);
+                fseek(arch, 0, SEEK_SET);
+		if(archSize != 0){
+			int Size = 0;
 			printf("Файлы в архиве:\n");
-			while (!feof(statInfo) && i != counter){
-				fscanf(statInfo, "%d %s", &arch[i].fileSize, arch[i].fileName);
-				printf("%s\n", arch[i].fileName);
-				archSize = archSize + arch[i].fileSize; 
-        			i++;
-    			}
-			printf("Размер архива: %d\n", archSize);
+			while(archSize > 0){
+                        	fscanf(arch, "%d %s", &bufFileSize, bufFileName);
+				printf("%s\n", bufFileName);
+				int counter = 1;
+				int bufSize = bufFileSize; 
+				while(bufSize / 10 > 0){
+					bufSize = bufSize /10;
+					counter++;
+				}
+				archSize = archSize - bufFileSize - strlen(bufFileName) - counter - 2;
+				for(int i = 0; i < bufFileSize; i++){
+                                	fgetc(arch);
+                                }
+				Size = Size + bufFileSize;
+			}
+			printf("Размер архива: %d\n", Size);
 		}
 		else{
 			printf("Файлов нет\n");
-			printf("Размер архива: 0\n");
+                        printf("Размер архива: 0\n");
 		}
-		fclose(statInfo);                                            
 	}
+	else
+		printf("Нет такого архива\n");
 }
 
 void help(){
-	printf("./archiver arch_name -i file1 : добавить файл в архив\n");
-	printf("./archiver arch_name -e file1 : удалить файл из архива\n");
-	printf("./archiver arch_name -s : вывести текущее состояние архива\n");
-	printf("./archiver arch_name -h file1 : вывести справку по работе с архиватором\n");
+	printf("./lab arch_name -i file1 : добавить файл в архив\n");
+	printf("./lab arch_name -e file1 : удалить файл из архива\n");
+	printf("./lab arch_name -s : вывести текущее состояние архива\n");
+	printf("./lab -h : вывести справку по работе с архиватором\n");
 }
 
 int main(int argc, char* argv[]){
@@ -231,8 +222,9 @@ int main(int argc, char* argv[]){
 	while ((rez = getopt(argc, argv, "i:e:sh")) != -1) {
 		switch (rez) {
 			case 'i': 
-				if(archName != 0) 
-					input(archName, optarg); 
+				if(archName != 0){
+					input(archName, optarg);
+				} 
 				else 
 					printf("Invalid archname\n"); 
 				break;
@@ -242,9 +234,12 @@ int main(int argc, char* argv[]){
 				else 
 					printf("Invalid archname\n"); 
 				break;
-			case 's':  
-				archStat(); 
-				break;
+			case 's': 
+				if(archName != 0)
+					archStat(archName);
+				else
+                                        printf("Invalid archname\n");
+                                break; 
 			case 'h': 
 				help(); 
 				break;
